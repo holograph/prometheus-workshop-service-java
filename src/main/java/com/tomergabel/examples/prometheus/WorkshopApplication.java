cea61dfd63d5f16af9e3d9bc5f0dfedc5c777c9e
Tomer Gabel <tomer@tomergabel.com>
2025-05-22 00:14:57 +0200

    Add metric endpoint + showcase custom buckets

--- src/main/java/com/tomergabel/examples/prometheus/WorkshopApplication.java
+++ src/main/java/com/tomergabel/examples/prometheus/WorkshopApplication.java
@@ -1,18 +1,39 @@
 package com.tomergabel.examples.prometheus;
 
 import io.opentelemetry.api.OpenTelemetry;
+import io.opentelemetry.exporter.prometheus.PrometheusHttpServer;
+import io.opentelemetry.sdk.autoconfigure.spi.AutoConfigurationCustomizerProvider;
+import io.opentelemetry.sdk.metrics.Aggregation;
+import io.opentelemetry.sdk.metrics.InstrumentSelector;
+import io.opentelemetry.sdk.metrics.View;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.convert.ApplicationConversionService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.format.Formatter;
-import org.springframework.format.FormatterRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @EnableAutoConfiguration
 public class WorkshopApplication {
+    @Bean AutoConfigurationCustomizerProvider autoConfigurationCustomizerProvider() {
+        return config -> config
+            .addMeterProviderCustomizer((meterProvider, props) -> meterProvider
+                .registerMetricReader(
+                    PrometheusHttpServer.builder()
+                        .setPort(props.getInt("prometheus.http.port", 8081))
+                        .build())
+                .registerView(
+                    InstrumentSelector.builder()
+                        .setMeterName("showcase")
+                        .build(),
+                    View.builder()
+                        .setAggregation(Aggregation.explicitBucketHistogram(List.of(0.0, 500.0, 1000.0, 10000.0)))
+                        .build()
+                )
+            );
+    }
+
     @Bean ShowcaseController showcaseController(OpenTelemetry openTelemetry) {
         return new ShowcaseController(openTelemetry);
     }
